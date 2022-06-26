47f6586543fb13fedec40aa9b9da98622201f6fa
김준욱 <wqsaxz6996@gmail.com>
2022-06-27 00:43:58 +0900

    Add getData function

--- app/src/main/java/com/example/bithumb_open_api/MainActivity.kt
+++ app/src/main/java/com/example/bithumb_open_api/MainActivity.kt
@@ -1,13 +1,103 @@
 package com.example.bithumb_open_api
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.bithumb_open_api.databinding.ActivityMainBinding
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+    private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+    private val coinList = arrayOf("BTC", "ETH", "ETC", "XRP", "BCH", "QTUM", "BTG", "EOS", "ICX",
+    "TRX", "ELF", "OMG", "KNC", "GLM", "ZIL", "WAXP", "POWR", "LRC", "STEEM", "STRAX", "ZRX",
+    "REP", "SNT", "ADA", "CTXC", "BAT", "THETA", "LOOM", "WAVES", "LINK", "ENJ", "VET", "MTL",
+    "IOST", "TMTG", "QKC", "ATOLO", "AMO", "BSV", "ORBS", "TFUEL", "VALOR", "CON", "ANKR", "MIX",
+    "CRO", "FX", "CHR", "MBL", "MXC", "FCT2", "TRV", "DAD", "WOM", "SOC", "BOA", "MEV", "SXP",
+    "COS", "EL", "BASIC", "HIVE", "XPR", "VRA", "FIT", "EGG", "BORA", "ARPA", "CTC", "APM", "CKB",
+    "AERGO", "ANW", "CENNZ", "EVZ", "CYCLUB", "SRM", "QTCON", "UNI", "YFI", "UMA", "AAVE", "COMP",
+    "REN", "BAL", "RSR", "NMR", "RLC", "UOS", "SAND", "GOM2", "BEL", "OBSR", "ORC", "POLA", "AWO",
+    "ADP", "DVI", "GHX", "MIR", "MVC", "BLY", "WOZX", "ANV", "GRT", "MM", "BIOT", "XNO", "SNX",
+    "SOFI", "COLA", "OXT", "LINA", "MAP", "AQT", "PLA", "WIKEN", "CTSI", "MANA", "LPT", "MKR",
+    "SUSHI", "ASM", "PUNDIX", "CELR", "ARW", "FRONT", "RLY", "OCEAN", "BFC", "ALICE", "OGN", "COTI",
+    "CAKE", "BNT", "XVS", "SWAP", "CHZ", "AXS", "DAO", "SIX", "DAI", "MATIC", "WOO", "ACH", "VELO",
+    "XLM", "VSYS", "IPX", "WICC", "ONT", "META", "KLAY", "ONG", "ALGO", "JST", "XTZ", "MLK", "WEMIX",
+    "DOT", "ATOM", "SSX", "TEMCO", "HIBS", "DOGE", "KSM", "CTK", "XYM", "BNB", "NFT", "SUN", "XEC",
+    "PCI", "SOL", "EGLD", "GO", "DFA", "C98", "MED", "1INCH", "BOBA", "GALA", "BTT", "EFI", "JASMY",
+    "TITAN", "REQ", "CSPR", "AVAX", "TDROP", "SPRT", "NPT", "REI", "T", "MBX", "GMT")
+    var infoList : MutableList<Map<String, String>> = mutableListOf(mapOf<String, String>("" to ""))
+    var clickedPosition = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val recyclerAdapter = RecyclerAdapter(coinList, infoList)
+        recyclerAdapter.setItemClickListener(object: RecyclerAdapter.OnItemClickListener{
+            override fun onClick(v: View, positon: Int) {
+                clickedPosition = positon
+                Log.d("position : ", clickedPosition.toString())
+            }
+        })
+//        recyclerAdapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+
+        getData(coinList, recyclerAdapter)
+
+        binding.btnRefresh.setOnClickListener {
+//            val state = binding.recyclerView.layoutManager?.onSaveInstanceState()
+            getData(coinList, recyclerAdapter)
+//            binding.recyclerView.layoutManager?.onRestoreInstanceState(state)
+            binding.recyclerView.scrollToPosition(clickedPosition)
+        }
+
+        Log.d("infoList", infoList.toString())
+    }
+
+    private fun getData(coinList: Array<String>, recyclerAdapter: RecyclerAdapter) {
+        val url = "https://api.bithumb.com/public/ticker/ALL_KRW"
+
+        val jsonRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            com.android.volley.Response.Listener<JSONObject> { response ->
+                infoList.clear()
+                for (x in coinList){
+                    val data = response.getJSONObject("data").get(x)
+                    val map = stringToMap(data.toString())
+                    addMapToList(map)
+                    Log.d("$x : ", "$data + ${data.javaClass.name}")
+                    Log.d("map", map.toString())
+                }
+                recyclerAdapter.notifyDataSetChanged()
+                setUpRecyclerView(recyclerAdapter)
+            },
+            com.android.volley.Response.ErrorListener { error ->
+                Log.d("Volley", error.toString()) })
+        val queue = Volley.newRequestQueue(this)
+        queue.add(jsonRequest)
+    }
+
+    private fun setUpRecyclerView(recyclerAdapter: RecyclerAdapter) {
+        binding.recyclerView.adapter = recyclerAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun addMapToList(map: Map<String, String>) {
+        infoList.add(map)
+    }
+
+    private fun stringToMap(string: String): Map<String, String> {
+        val processedString = string.replace("{", "").replace("}", "").replace("\"", "")
+        val map = processedString.split(",").associate{
+            val (left, right) = it.split(":")
+            left to right
+        }
+        return map
     }
 }
---
+++ app/src/main/java/com/example/bithumb_open_api/RecyclerAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.bithumb_open_api
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bithumb_open_api.databinding.CardviewBinding
+
+class RecyclerAdapter(private val coinList : Array<String>, private val infoList : MutableList<Map<String, String>>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    inner class MyViewHolder(val binding : CardviewBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder = MyViewHolder(
+        CardviewBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding =(holder as RecyclerAdapter.MyViewHolder).binding
+        with(binding){
+            textName.text = coinList[position]
+            textOpening.text = infoList[position]["opening_price"]
+            textClosing.text = infoList[position]["closing_price"]
+            textMin.text = infoList[position]["min_price"]
+            textMax.text = infoList[position]["max_price"]
+            textUnitTraded.text = infoList[position]["units_traded"]
+            textTradeValue.text = infoList[position]["acc_trade_value"]
+            textPrevClosing.text = infoList[position]["prev_closing_price"]
+            textUnitTraded24H.text = infoList[position]["units_traded_24H"]
+            textTradeValue24H.text = infoList[position]["acc_trade_value_24H"]
+            textFluctate24H.text = infoList[position]["fluctate_24H"]
+            textFluctateRate24H.text = infoList[position]["fluctate_rate_24H"]
+        }
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, holder.layoutPosition)
+//            Log.d("holder position : ", holder.layoutPosition.toString())
+        }
+    }
+
+    interface OnItemClickListener{
+        fun onClick(v: View, positon: Int)
+    }
+
+    fun setItemClickListener(onItemClickListener: OnItemClickListener){
+        this.itemClickListener = onItemClickListener
+    }
+
+    private lateinit var itemClickListener : OnItemClickListener
+
+    override fun getItemCount(): Int {
+        return coinList.size
+    }
+
+}
