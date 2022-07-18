38ee1837a971ecccc71632e0ff30b0361a75dd40
JunWook Kim <qwaszx6996@naver.com>
2022-07-18 18:31:16 +0900

    Add theme(light & dark)

--- app/src/main/java/com/example/bithumb_open_api/DetailActivity.kt
+++ app/src/main/java/com/example/bithumb_open_api/DetailActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bithumb_open_api
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
